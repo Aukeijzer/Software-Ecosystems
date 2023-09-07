4918fb6750c084ad065fa37dff669e88789b7260
Barrel1 <daniogaming276@gmail.com>
2023-09-07 14:08:33 +0200

    Front-end Initial version
    Blank sheet for front-end. Just a simple landing page.

---
+++ front-end/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      Hello world, this is the ecoDashboard
+      <br></br>
+      There should be some data here about various gitHub thingies.
+    </div>
+  )
+}
