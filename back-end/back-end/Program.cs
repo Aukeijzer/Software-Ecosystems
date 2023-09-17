5831314a1ab693bd335c5b1caad49c072d430f4b
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-09-17 22:04:04 +0200

    refactor:  change target framework from .NET Framework 4.8 to .NET Core 7.0

--- back-end/back-end/Program.cs
+++ back-end/back-end/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace back_end
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
