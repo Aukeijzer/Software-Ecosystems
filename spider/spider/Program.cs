d474453484094403c3d18e1b9c5653c440767d8a
SimonHol <s.hol@students.uu.nl>
2023-09-19 16:54:22 +0200

    fix: new project for newer C#
    Since version of C# (and .NET frameworks) were outdated, make new project with correct dependecies. Also start of adding API to enable communication with the backend.

--- spider/spider/Program.cs
+++ spider/spider/Program.cs
@@ -1,181 +1,25 @@
-﻿using GraphQL.Client.Serializer.Newtonsoft;
-using GraphQL.Client.Abstractions;
-using GraphQL.Client.Http;
-using GraphQL.Client;
-using System.Net;
-using System;
-using System.Collections.Generic;
-using System.Net.Mime;
-using System.Text.Json;
-using GraphQL.Client.Serializer.SystemTextJson;
-using Newtonsoft.Json;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace spider
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    internal class Program
-    {
-        private const string Token = "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";
-        private const string UserLogin = "Secodash";
-        //private static readonly ProductHeaderValue ProductInformation = new ProductHeaderValue("Git_Test_Repo", "1.0");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-        public static void Main(string[] args)
-        {
-            var graphQLClient = new GraphQLHttpClient("https://api.github.com/graphql", new SystemTextJsonSerializer());
-            graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + Token);
-            //Connection conn = new Connection(ProductInformation, Token);
-            RunQuery(graphQLClient);
-            //RateLimitCheck(conn);
-            Console.ReadLine();
-        }
+app.UseHttpsRedirection();
 
-        private static async void RunQuery(GraphQLHttpClient client)
-        {
-            /*
-            var repositoriesQuery = new Query()
-                .Search(query: "API_Test_Repo", SearchType.Repository, first: 1)
-                .Select(x=> new
-                {
-                    x.RepositoryCount,
-                    nds = x.Nodes.Select(node => new
-                    {
-                        NodeData = node.Switch<RepositoryData>(
-                            when => when.Repository(
-                            repo => new RepositoryData(
-                                repo.Name,
-                                repo.Owner.Login
-                                //repo.Id
-                                )))
-                    }).ToList()
+app.UseAuthorization();
 
-                });
-            */
-            
-            var repositoriesQuery = new GraphQLHttpRequest()
-            {
-                Query = @"query repositoriesQueryRequest($name: String!, $fileName : String!){
-                            search(query: $name, type: REPOSITORY, first: 10) {
-                            repositoryCount
-                             nodes {
-                                ... on Repository {
-                                    name
-                                    owner{
-                                        login
-                                    }
-                                    repositoryTopics(first: 10){
-                                        nodes{
-                                            topic{
-                                            name
-                                            }
-                                        }
-                                    }
-                                    object(expression: $fileName){
-                                        ... on Blob{
-                                            text
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }",
-                OperationName = "repositoriesQueryRequest",
-                Variables = new{name="API_Test_Repo", fileName = "main:README.md"}
-            };
+app.MapControllers();
 
-            var result = await client.SendQueryAsync<Object>(repositoriesQuery);
-            Console.WriteLine(result.Data);
-            var nds = "";
-            foreach (var nde in nds)
-            {
-            /*
-            var res = nde.NodeData;
-            //Console.WriteLine(res.ToString());
-            var repositoryReadMeQuery = new Query()
-                .Repository( res.Name, res.Owner )
-                .Object(expression: "main:README.md")
-                .Select(x => x);
-            */
-
-            var repositoryReadMeQuery = new GraphQLHttpRequest()
-            {
-                Query = @"query RepositoryReadMeQueryRequest($name : String!, $owner : String!, $fileName : String!) {
-                                repository(name: $name, owner: $owner){
-                                    name
-                                    owner{
-                                        login
-                                    }
-                                    object(expression: $fileName){
-                                        ... on Blob{
-                                            text
-                                            }
-                                        }
-                                    }
-                                }",
-                OperationName = "RepositoryReadMeQueryRequest",
-                Variables = new { name = "API_Test_Repo", owner = "Secodash", fileName = "main:README.md" }
-            };
-
-            //var repositoryReadMeQueryResponse = await client.SendQueryAsync(repositoryReadMeQuery, () => new {Data = new RepoReadMeData() });
-            var repositoryReadMeQueryResponse = await client.SendQueryAsync<Object>(repositoryReadMeQuery);
-            Console.WriteLine(repositoryReadMeQueryResponse.Data.ToString()); 
-            }
-
-        }
-
-        private static async void RateLimitCheck(GraphQLHttpClient client)
-        {
-            var RateLimitCheck = new GraphQLHttpRequest()
-            {
-                Query = @"query rateLimit", OperationName = "RateLimitCheck"
-            };
-            var response = await client.SendQueryAsync<string>(RateLimitCheck);
-            Console.WriteLine(response.Data);
-        }
-        /*
-        class RepositoryData
-        {
-            public string Name { get; set; }
-
-            public string Owner { get; set; }
-
-            //private readonly ID _id;
-            public RepositoryData(string name, string owner , ID id)
-            {
-                this.Name = name;
-                this.Owner = owner;
-                //this._id = id;
-            }
-
-            public override string ToString()
-            {
-                return "Name: " + Name + " || " + "Owner: " + Owner; // + " || ID: " + _id;
-            }
-        }
-        */
-        class RepoReadMeDataWrapper
-        {
-            public RepoReadMeData Data { get; set; }
-        }
-        
-        }
-        class RepoReadMeData
-        {
-            public string Name { get; set; }
-            public string Owner{ get; set; }
-
-            public string ReadMeText{ get; set; }
-            
-            /*
-            public RepoReadMeData(string name, string owner, string text)
-            {
-                this.Name = name;
-                this.Owner = owner;
-                this.ReadMeText = text;
-            }
-            */
-            
-            public override string ToString()
-            {
-                return "Name: " + Name + " || " + "Owner: " + Owner + " || Text: " + ReadMeText;
-            }
-        }
-    }
+app.Run();
