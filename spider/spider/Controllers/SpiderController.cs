495cbea22557da4f9ccb2a70fd16b203b9fa04a3
SimonHol <s.hol@students.uu.nl>
2023-09-20 10:56:10 +0200

    refactor: parse returned data object to a C# object
    The JSON object returned from the query parsed to a C# object instead, with the option to return using the API.

---
+++ spider/spider/Controllers/SpiderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using spider;
+
+namespace spider.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SpiderController
+{
+    [HttpGet(Name = "GetQueryData")]
+    public SpiderData Get()
+    {
+
+        return Spider.Main();
+    }
+}
