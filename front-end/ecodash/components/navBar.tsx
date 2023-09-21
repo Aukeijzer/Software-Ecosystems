fe12b484a97983fa5d5b09356791b06fd0931480
Barrel1 <daniogaming276@gmail.com>
2023-09-21 15:34:21 +0200

    feat: add cypress component testing
    This commit adds cypress component testing,
    for now not that many interesting tests are in place :)

--- front-end/ecodash/components/navBar.tsx
+++ front-end/ecodash/components/navBar.tsx
@@ -4,8 +4,10 @@
 import NavLogo from "./navLogo"
 
 export default function NavBar(){
+    
+    
     return(
-        <nav className="bg-blue-300 border-gray-200 max-w">
+        <nav className="bg-blue-300 border-gray-200 max-w" data-cy="navBar">
             <NavLogo />
         </nav>
     )
