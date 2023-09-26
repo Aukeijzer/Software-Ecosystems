902c2f65b6edae9acc4caed2d2a28050d91ab54c
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-09-26 13:23:23 +0200

    feat(backend): add Quantum ecosystem to database initializer

--- back-end/back-end/Database/DbInitializer.cs
+++ back-end/back-end/Database/DbInitializer.cs
@@ -15,15 +15,15 @@ public static class DbInitializer
          DisplayName = "Agriculture",
          Description = "Software related to agriculture",
          NumberOfStars = 34565,
-         Projects = new List<Project>
-         {
-            new()
-            {
-               Name = "awesome-agriculture", About = "Open source technology for agriculture, farming, and gardening",
-               Owner = "brycejohnston", NumberOfStars = 1100,
-               ReadMe = "A curated list of awesome open source technology for agriculture, farming, and gardening."
-            },
-         }
+         Projects = new List<Project>(),
+      });
+      context.Ecosystems.Add(new Ecosystem
+      {
+         Name = "quantum",
+         DisplayName = "Quantum",
+         Description = "Software related to quantum mechanics",
+         NumberOfStars = 4352,
+         Projects = new List<Project>(),
       });
       context.SaveChanges();
    }
