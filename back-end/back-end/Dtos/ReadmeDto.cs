9f1935ce6cee7295dc097d74e80ca489194b91be
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-09-28 15:32:49 +0200

    feat(backend): add Topic model, ProjectTopicsDto and ReadmeDto

---
+++ back-end/back-end/Dtos/ReadmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace SECODashBackend.Dtos;
+
+/// <summary>
+/// Data Transfer Object for a readme
+/// </summary>
+[DataContract]
+public class ReadmeDto
+{
+    [DataMember(Name = "projectId")]
+    public required long ProjectId { get; init; }
+    
+    [DataMember(Name = "readme")]
+    public required string Readme { get; init; }
+}
---
+++ back-end/back-end/Models/Topic.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace SECODashBackend.Models;
+[DataContract]
+public class Topic
+{
+    [DataMember(Name ="id")]
+    public int? Id { get; set; }
+    
+    [DataMember(Name = "label")]
+    public string? Label { get; set; }
+    
+    [DataMember(Name = "keywords")]
+    public required List<string> Keywords { get; init; }
+    
+    [DataMember(Name = "probability")]
+    public required float Probability { get; init; }
+}
