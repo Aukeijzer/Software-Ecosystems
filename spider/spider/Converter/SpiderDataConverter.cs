779af838381322976c3410aa9a68e1838695971c
Quentin Kopp <q.f.kopp@students.uu.nl>
2023-09-30 15:21:12 +0200

    refactor(spider): Put duplicate code in a separate method

--- spider/spider/Converter/SpiderDataConverter.cs
+++ spider/spider/Converter/SpiderDataConverter.cs
@@ -8,32 +8,30 @@ public class SpiderDataConverter : ISpiderDataConverter
     public List<Project> ToProjects(SpiderData data)
     {
         // Parser to parse the result from the search query to a C# data type.
-        var projects = new List<Project>();
-        foreach (var repository in data.search.nodes)
-        {
-            projects.Add(new Project()
-            {
-                Name = repository.name,
-                ReadMe = (repository.readme == null ? null : repository.readme.text) ,
-                Owner = repository.owner.login
-            });
-        }
+        List<Project> projects = DataToProjects(data.search.nodes);
         return projects;
     }
-
+    
     public List<Project> TopicSearchToProjects(TopicSearchData data)
     {
-        
+        List<Project> projects = DataToProjects(data.topic.repositories.nodes);
+        return projects;
+    }
+    
+    private List<Project> DataToProjects(Repository[] nodes)
+    {
         var projects = new List<Project>();
-        foreach (var repository in data.topic.repositories.nodes)
+        foreach (var repository in nodes)
         {
             projects.Add(new Project()
             {
                 Name = repository.name,
-                ReadMe = (repository.readme == null ? null : repository.readme.text) ,
-                Owner = repository.owner.login
+                ReadMe = (repository.readme == null ? null : repository.readme.text),
+                Owner = repository.owner.login,
+                Description = repository.description
             });
         }
+
         return projects;
     }
 }
