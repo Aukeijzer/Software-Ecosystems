d3ac915cb8b21ab9778af328e791112dbed603f5
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-03 15:45:16 +0200

    feat(backend): add dtos for ProgrammingLanguage and Project

---
+++ back-end/back-end/Dto/ProjectDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace SECODashBackend.Dto;
+
+[DataContract]
+public class ProjectDto
+{
+   [Required]
+   [DataMember(Name = "id")]
+   public required string Id { get; init; }
+   
+   [Required]
+   [DataMember(Name = "name")]
+   public required string Name { get; set; }
+   
+   [DataMember(Name = "owner")]
+   public string? Owner { get; set; }
+   
+   [DataMember(Name = "description")]
+   public string? Description { get; set; }
+   
+   [DataMember(Name = "topics")]
+   public string[]? Topics { get; set; }
+
+   [DataMember(Name = "languages")]
+   public List<ProgrammingLanguageDto> Languages { get; set; } = new List<ProgrammingLanguageDto>();
+   
+   [DataMember(Name = "totalSize")]
+   public int? TotalSize { get; set; }
+
+   [DataMember(Name = "readme")]
+   public string? ReadMe { get; set; }
+   
+   [DataMember(Name = "numberOfStars")]
+   public int? NumberOfStars { get; set; }
+}
