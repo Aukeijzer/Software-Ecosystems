fe7c9421e5bfbfe9c804fd7474122a1dbd1281e0
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-03 15:58:52 +0200

    feat(backend): add ProjectConverter for converting Projects to Dtos and vice versa

---
+++ back-end/back-end/DataConverter/ProjectConverter.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.JSInterop.Infrastructure;
+using SECODashBackend.Dto;
+using SECODashBackend.Enums;
+using SECODashBackend.Models;
+
+namespace SECODashBackend.DataConverter;
+
+public static class ProjectConverter
+{
+    public static Project ToProject(ProjectDto dto)
+    {
+        var project = new Project()
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            Description = dto.Description,
+            Languages = new List<ProjectProgrammingLanguage>(dto.Languages.Select(ToProjectProgrammingLanguage)),
+            NumberOfStars = dto.NumberOfStars,
+            Owner = dto.Owner,
+            ReadMe = dto.ReadMe
+        };
+        return project;
+    }
+
+    public static ProjectDto ToDto(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static ProjectProgrammingLanguage ToProjectProgrammingLanguage(ProgrammingLanguageDto dto)
+    {
+        return new ProjectProgrammingLanguage()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Language = ParseProgrammingLanguage(dto.Name),
+            Percentage = dto.Percentage
+        };
+    }
+
+    private static ProgrammingLanguage ParseProgrammingLanguage(string language)
+    {
+        try
+        {
+            return Enum.Parse<ProgrammingLanguage>(language);
+        }
+        catch (ArgumentException e)
+        {
+            return language switch
+            {
+                "C++" => ProgrammingLanguage.CPlusPlus,
+                "C#" => ProgrammingLanguage.CSharp,
+                "DIGITAL Command Language" => ProgrammingLanguage.DIGITALCommandLanguage,
+                "Q#" => ProgrammingLanguage.QSharp,
+                _ => ProgrammingLanguage.Undefined,
+            };
+        }
+    }
+}
