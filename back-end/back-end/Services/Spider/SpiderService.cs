9173615d52096e0670ff097dbe23861b7ee6eac4
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-04 14:46:53 +0200

    fix(backend): fix methods of SpiderService using the wrong routes

--- back-end/back-end/Services/Spider/SpiderService.cs
+++ back-end/back-end/Services/Spider/SpiderService.cs
@@ -1,4 +1,4 @@
-﻿using RestSharp;
+using RestSharp;
 using SECODashBackend.Dto;
 
 namespace SECODashBackend.Services.Spider;
@@ -14,13 +14,13 @@ public class SpiderService : ISpiderService
     }
     public async Task<List<ProjectDto>> GetProjectsByKeywordAsync(string keyword)
     {
-        var request = new RestRequest("topic/" + keyword);
+        var request = new RestRequest("name/" + keyword);
         // Throw an exception if the request cannot be deserialized into a List of Projects
         return await _spiderClient.GetAsync<List<ProjectDto>>(request) ?? throw new HttpRequestException();
     }
     public async Task<List<ProjectDto>> GetProjectsByTopicAsync(string topic)
     {
-        var request = new RestRequest("name/" + topic);
+        var request = new RestRequest("topic/" + topic);
         // Throw an exception if the request cannot be deserialized into a List of Projects
         return await _spiderClient.GetAsync<List<ProjectDto>>(request) ?? throw new HttpRequestException();
     }
