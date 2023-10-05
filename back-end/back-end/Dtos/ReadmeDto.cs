9f6affdc0289e6835a099af2f0d8e8b7a5d60183
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-05 15:13:19 +0200

    feat(backend): add Dto for sending readmes to data-processor

--- back-end/back-end/Dtos/ReadmeDto.cs
+++ back-end/back-end/Dtos/ReadmeDto.cs
@@ -9,7 +9,7 @@ namespace SECODashBackend.Dtos;
 public class ReadmeDto
 {
     [DataMember(Name = "projectId")]
-    public required long ProjectId { get; init; }
+    public required string ProjectId { get; init; }
     
     [DataMember(Name = "readme")]
     public required string Readme { get; init; }
