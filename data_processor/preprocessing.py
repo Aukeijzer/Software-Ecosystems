2409f8d2b7a9ec315aaa1ff66ef6a92ce42d68f8
M4t34n <57446392+M4t34n@users.noreply.github.com>
2023-10-09 15:20:10 +0200

    feat(data-processor): add preprocessor

--- data_processor/preprocessing.py
+++ data_processor/preprocessing.py
@@ -1,3 +1,6 @@
+import markdown
+from bs4 import BeautifulSoup
+
 # Preprocessing data
 
 def preprocessDocs(docs):
@@ -5,6 +8,7 @@ def preprocessDocs(docs):
     return preprocessed_documents
 
 
-def preprocess_document(document):
-    processed_document = document
+def preprocess_document(document):    
+    html_document = markdown.markdown(document)
+    processed_document = ''.join(BeautifulSoup(html_document).findAll(text=True))
     return processed_document
