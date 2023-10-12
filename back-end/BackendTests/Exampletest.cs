1dd834592bee4371ad74766eb9b79322d1a709f8
Aukeijzer <auke.roest01@gmail.com>
2023-10-12 18:12:33 +0200

    test(backend): remove unnescessary test

--- back-end/BackendTests/Exampletest.cs
+++ back-end/BackendTests/Exampletest.cs
@@ -2,18 +2,14 @@ namespace BackendTests;
 
 public class ExampleTests
 {
-    Ecosystem projectsService;
     [SetUp]
     public void Setup()
     {
-        //Test fails if this is commented out
-        projectsService = new Ecosystem();
     }
 
     // Example test
     [Test]
     public void ExampleTest()
     {
-        Assert.DoesNotThrow(() => projectsService.GetHashCode());
     }
 }
