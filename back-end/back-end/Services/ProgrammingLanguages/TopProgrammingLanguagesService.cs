3bd1f2983e34163102a070c3bef3bcd4fb34b615
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-12 10:57:08 +0200

    style(backend): split LINQ statements over multiple lines

--- back-end/back-end/Services/ProgrammingLanguages/TopProgrammingLanguagesService.cs
+++ back-end/back-end/Services/ProgrammingLanguages/TopProgrammingLanguagesService.cs
@@ -1,4 +1,4 @@
-using SECODashBackend.Enums;
+﻿using SECODashBackend.Enums;
 using SECODashBackend.Models;
 
 namespace SECODashBackend.Services.ProgrammingLanguages;
@@ -12,28 +12,37 @@ public static class TopProgrammingLanguagesService
     {
        
         // Get the list of languages from the projects in the ecosystem and flatten it
-        var languages = ecosystem.Projects.Select(p => p.Languages).SelectMany(l => l);
+        var languages = ecosystem.Projects
+            .Select(p => p.Languages)
+            .SelectMany(l => l);
+        
         // Group the languages by their name and sum their percentages and remove duplicates
-        var groupedLanguages = languages.GroupBy(l => l.Language).Select(l => new EcosystemProgrammingLanguage()
-        {
-            EcosystemId = ecosystem.Id,
-            Language = l.Key,
-            Percentage = l.Sum(p => p.Percentage)
-        }).Distinct();
+        var groupedLanguages = languages
+            .GroupBy(l => l.Language)
+            .Select(l => new EcosystemProgrammingLanguage
+                {
+                    EcosystemId = ecosystem.Id,
+                    Language = l.Key,
+                    Percentage = l.Sum(p => p.Percentage)
+                }).Distinct();
         
         // Get the total sum of all percentages
         float total = groupedLanguages.Select(l => l.Percentage).Sum();
 
-        var fixedPercentages = groupedLanguages.Select(l => new EcosystemProgrammingLanguage()
-        {
-            EcosystemId = l.EcosystemId,
-            Language = l.Language,
-            Percentage = float.Round(l.Percentage / total * 100)
-        });
+        var fixedPercentages = groupedLanguages
+            .Select(l => new EcosystemProgrammingLanguage
+                {
+                    EcosystemId = l.EcosystemId,
+                    Language = l.Language,
+                    Percentage = float.Round(l.Percentage / total * 100)
+                });
         
         // Order the languages by their percentage in descending order
-        var orderedLanguages = fixedPercentages.OrderByDescending(l => l.Percentage).Take(NumberOfLanguages);
-        var other = new EcosystemProgrammingLanguage()
+        var orderedLanguages = fixedPercentages
+            .OrderByDescending(l => l.Percentage)
+            .Take(NumberOfLanguages);
+        
+        var other = new EcosystemProgrammingLanguage
         {
             EcosystemId = ecosystem.Id,
             Language = ProgrammingLanguage.Other,
