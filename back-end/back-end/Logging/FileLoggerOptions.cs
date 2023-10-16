f57c426ecd577b38bf9441bf60a5e596f9c399c8
Quentin Kopp <71125578+Mique17@users.noreply.github.com>
2023-10-16 14:14:11 +0200

    Sprint03/#75 implement logging in spider and back end (#22)
    * Implement file logging for back-end and spider
    
    * Add log messages to ProjectsController
    
    * Add log messages to additional SpiderController functions
    
    * Fix faulty messages
    
    * chore: remove unnecessary gitignore
    
    * chore: update gitignore to include log files
    
    * chore: remove unnecessary gitignore line
    
    * chore: fix gitignore
    
    * chore: gitignore working
    
    * chore: fix gitignore
    
    * chore: fix gitignore
    
    * chore: fix gitignore
    
    * chore: fix gitignore
    
    * chore: fix gitignore
    
    * refactor(logging): line width
    
    * refactor(logging): line width
    
    * refactor(logging): line width
    
    ---------
    
    Co-authored-by: ian van uunen <i.r.vanuunen@students.uu.nl>
    Co-authored-by: SimonHol <45420284+SimonHol@users.noreply.github.com>

---
+++ back-end/back-end/Logging/FileLoggerOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SECODashBackend.Logging;
+
+public class FileLoggerOptions
+{
+    public virtual string FilePath { get; set; }
+    
+    public virtual string FolderPath { get; set; }
+}
---
+++ spider/spider/Logging/FileLogger.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace spider.Logging;
+
+public class FileLogger : ILogger 
+{
+    protected readonly FileLoggerProvider _fileLoggerProvider;
+
+    public FileLogger([NotNull] FileLoggerProvider fileLoggerProvider)
+    {
+        _fileLoggerProvider = fileLoggerProvider;
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel != LogLevel.None;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState,
+        Exception?, string> formatter)
+    {
+        if (!IsEnabled(logLevel))
+        {
+            return;
+        }
+
+        var fullFilePath = string.Format("{0}/{1}", _fileLoggerProvider.Options.FolderPath,
+            _fileLoggerProvider.Options.FilePath.Replace("{date}",
+                DateTime.Now.ToString("yyyyMMdd")));
+        var logRecord = string.Format("{0} [{1}] {2} {3}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+            logLevel.ToString(), formatter(state, exception), (exception != null ? exception.StackTrace : ""));
+        using (var streamWriter = new StreamWriter(fullFilePath, true))
+        {
+           streamWriter.WriteLine(logRecord);
+        }
+    }
+}
