12ff5742c0f86e5382139e722e072e2d953ba2eb
Quentin Kopp <q.f.kopp@students.uu.nl>
2023-10-16 17:18:47 +0200

    feat(spider): wip add a rest api request for contributors of a repo

--- spider/spider/Dtos/ContributorDto.cs
+++ spider/spider/Dtos/ContributorDto.cs
@@ -1,6 +1,16 @@
-﻿namespace spider.Dtos;
+﻿using System.Runtime.Serialization;
 
+namespace spider.Dtos;
+
+[DataContract]
 public class ContributorDto
 {
-    
+    [DataMember(Name = "login")]
+    public required string Login { get; init; }
+    [DataMember(Name = "id")]
+    public required int Id { get; init; }
+    [DataMember(Name = "nodeId")]
+    public required string NodeId { get; init; }
+    [DataMember(Name = "contributions")]
+    public required int Contributions { get; init; }
 }
--- spider/spider/Services/GithubRestService.cs
+++ spider/spider/Services/GithubRestService.cs
@@ -6,16 +6,16 @@ namespace spider.Services;
 
 public class GithubRestService : IGithubRestService
 {
-    private readonly RestClient _GithubRestClient;
+    private readonly RestClient _githubRestClient;
     public GithubRestService()
     {
         var options = new RestClientOptions("https://api.github.com");
-        _GithubRestClient = new RestClient(options);
+        _githubRestClient = new RestClient(options);
     }
 
     public async Task<Contributors> GetRepoContributors(String ownerName, string repoName)
     {
         var request = new RestRequest("repos/" + ownerName + "/" + repoName + "/contributors");
-        return await _GithubRestClient.GetAsync<Contributors>(request) ?? throw new HttpRequestException();
+        return await _githubRestClient.GetAsync<Contributors>(request) ?? throw new HttpRequestException();
     }
 }
