0f9b33c7512d7e7da09084c222cc18483247da56
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-23 16:53:38 +0200

    refactor(backend): add EcosystemConverter

---
+++ back-end/back-end/DataConverters/EcosystemConverter.cs
@@ -0,0 +1,19 @@
+﻿using SECODashBackend.Dtos.Ecosystem;
+using SECODashBackend.Models;
+
+namespace SECODashBackend.DataConverters;
+
+public static class EcosystemConverter
+{
+    public static EcosystemDto ToDto(Ecosystem ecosystem)
+    {
+        return new EcosystemDto
+        {
+            Id = ecosystem.Id,
+            Description = ecosystem.Description,
+            DisplayName = ecosystem.DisplayName,
+            Name = ecosystem.Name,
+            NumberOfStars = ecosystem.NumberOfStars,
+        };
+    }
+}
