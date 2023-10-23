081ef386ebd44aff5222d7db1640c3959301466b
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-23 16:53:07 +0200

    refactor(backend): add EcosystemDto

---
+++ back-end/back-end/Dtos/Ecosystem/EcosystemDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using SECODashBackend.Models;
+
+namespace SECODashBackend.Dtos.Ecosystem;
+
+public class EcosystemDto
+{
+   [DataMember(Name = "id")]
+   public required string Id { get; init; }
+   
+   [DataMember(Name = "name")]
+   public required string Name { get; init; }
+   
+   [DataMember(Name = "displayName")]
+   public string? DisplayName { get; init; }
+   
+   [DataMember(Name ="description")]
+   public string? Description { get; init; }
+
+   [Required]
+   [DataMember(Name = "projects")]
+   public IEnumerable<ProjectDto>? Projects { get; set; }
+   
+   [DataMember(Name = "numberOfStars")]
+   public int? NumberOfStars { get; init; }
+   
+   [DataMember(Name = "topLanguages")]
+   public List<EcosystemProgrammingLanguage> TopLanguages { get; set; } = new();
+}
