81a0ca8142350138100032c8b744572d30222485
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-23 17:50:16 +0200

    ops(backend): update database schema after removing Project DbSet
    BREAKING CHANGE: requires new develop database

--- back-end/back-end/Migrations/20231023154748_initialDb.Designer.cs
+++ back-end/back-end/Migrations/20231023154748_initialDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SECODashBackend.Database;
@@ -11,9 +12,11 @@ using SECODashBackend.Database;
 namespace SECODashBackend.Migrations
 {
     [DbContext(typeof(EcosystemsContext))]
-    partial class EcosystemsContextModelSnapshot : ModelSnapshot
+    [Migration("20231023154748_initialDb")]
+    partial class initialDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
