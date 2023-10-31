222864d5797874847899598260acf4bafe91de45
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-31 14:48:28 +0100

    refactor(backend): add EcosystemRequestDto

---
+++ back-end/back-end/Dtos/Ecosystem/EcosystemRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace SECODashBackend.Dtos.Ecosystem;
+
+public class EcosystemRequestDto
+{
+    [DataMember(Name = "topics")] public required List<string> Topics { get; init; }
+    [DataMember(Name = "numberOfTopLanguages")] public int? NumberOfTopLanguages { get; set; }
+    [DataMember(Name = "numberOfSubEcosystems")] public int? NumberOfTopSubEcosystems { get; set; }
+    [DataMember(Name = "numberOfTopContributors")] public int? NumberOfTopContributors { get; set; }
+}
