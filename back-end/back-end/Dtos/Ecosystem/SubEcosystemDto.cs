b260f5d798542e807eafe4ec89b673bffb32f5e2
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-10-31 14:47:33 +0100

    refactor(backend): add SubEcosystemDto

---
+++ back-end/back-end/Dtos/Ecosystem/SubEcosystemDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.Serialization;
+
+namespace SECODashBackend.Dtos.Ecosystem;
+
+public class SubEcosystemDto
+{
+    [DataMember(Name = "topics")] public required List<string> Topics { get; init; }
+    [DataMember(Name = "projectCount")] public int ProjectCount { get; init; }
+}
