9cf945938b2423114f090d7251d89d6b1daa3bf8
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-11-15 16:50:50 +0100

    style(backend): remove trailing comma

--- back-end/BackendTests/ElasticsearchAnalysisServiceTests.cs
+++ back-end/BackendTests/ElasticsearchAnalysisServiceTests.cs
@@ -42,7 +42,7 @@ public class ElasticsearchAnalysisServiceTests
             {
                 Language = "JavaScript",
                 Percentage = 80
-            },
+            }
         };
         
         // Act
