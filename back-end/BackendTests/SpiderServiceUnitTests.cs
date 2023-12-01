cecabcdd02072a1aa22cd95c8f5887d583705b9e
Vivian <viv16mei@gmail.com>
2023-12-01 16:40:10 +0100

    refactor: added unit tests for the spider service testing mining by topic and keyword. Achieved 100% as test coverage for these methods.

--- back-end/BackendTests/SpiderServiceUnitTests.cs
+++ back-end/BackendTests/SpiderServiceUnitTests.cs
@@ -1,23 +1,51 @@
 ﻿using SECODashBackend.Dtos.Project;
+using SECODashBackend.Services.Spider;
 
 namespace BackendTests;
 [TestFixture]
 public class SpiderServiceUnitTests
 {
-    private readonly TSpiderService testSpiderService = new TSpiderService();
+    private readonly SpiderService testSpiderService = new SpiderService("http://localhost:5205/Spider");
     
+    /// <summary>
+    /// This tests the GetProjectsByTopicAsync method of the SpiderService.
+    /// </summary>
     [Test]
-    public void GetProjectsByTopicAsync_ReturnsEmptyList()
+    public void GetProjectsByTopicAsync_ReturnsProjects()
     {
         // Arrange
-        string testTopic = "test";
-        int amount = 1;
+        string testTopic = "ai";
+        int amount = 10;
         
         // Act
         Task<List<ProjectDto>> result = testSpiderService.GetProjectsByTopicAsync(testTopic, amount);
         
         // Assert
-        Assert.That(result, Is.Not.Null);
+        var returnList = result.Result;
+        Assert.That(result.Exception, Is.Null);
+        Assert.That(returnList, Is.Not.Null);
+        Assert.That(returnList.Count, Is.EqualTo(amount));
+        Assert.IsNotEmpty(returnList.FindAll(p => p.Topics.Contains(testTopic)));
+    }
+    
+    /// <summary>
+    /// This tests the GetProjectsByKeywordAsync method of the SpiderService.
+    /// </summary>
+    [Test]
+    public void GetProjectsByKeywordAsync_ReturnsProjects()
+    {
+        // Arrange
+        string testKeyword = "quantum";
+        int amount = 10;
         
+        // Act
+        Task<List<ProjectDto>> result = testSpiderService.GetProjectsByKeywordAsync(testKeyword, amount);
+        
+        // Assert
+        var returnList = result.Result;
+        Assert.That(result.Exception, Is.Null);
+        Assert.That(returnList, Is.Not.Null);
+        Assert.That(returnList.Count, Is.EqualTo(amount));
+        Assert.IsNotEmpty(returnList.FindAll(p => p.Name.Contains(testKeyword)));
     }
 }
