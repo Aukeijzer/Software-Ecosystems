5b5d559750d09133eac4ffb612480d9026b9c44b
Barrel1 <daniogaming276@gmail.com>
2023-12-11 15:42:28 +0100

    style: (frontend) add documentation
    add documentation to authOptions and next api route

--- front-end/ecodash/app/api/auth/[...nextauth]/route.ts
+++ front-end/ecodash/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/app/utils/authOptions";
 
-
+/**
+ * Handles the authentication route using NextAuth.
+ * @param {NextAuthOptions} authOptions - The authentication options.
+ * @returns {NextApiHandler} - The Next.js API handler.
+ */
 const handler = NextAuth(authOptions);
 export {  handler as GET, handler as POST}
--- front-end/ecodash/app/utils/authOptions.ts
+++ front-end/ecodash/app/utils/authOptions.ts
@@ -10,20 +10,24 @@ console.log(hostName);
 
 export interface ExtendedUser extends User {
     isAdmin: boolean;
-    userId: string;
 }
 
+/**
+ * Configuration options for authentication in the application.
+ */
 export const authOptions: NextAuthOptions = {
     
-    //Add callback here
-
     providers: [
+        /**
+         * Google authentication provider configuration.
+         */
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            
-
         }),
+        /**
+         * GitHub authentication provider configuration.
+         */
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!
@@ -31,11 +35,16 @@ export const authOptions: NextAuthOptions = {
     ],
 
     session: {
+        /**
+         * Session strategy configuration.
+         */
         strategy: "jwt",
-        
     },
     cookies: {
         sessionToken: {
+            /**
+             * Name and options for the session token cookie.
+             */
             name: `${cookiePrefix}next-auth.session-token`,
             options: {
               httpOnly: true,
@@ -43,55 +52,44 @@ export const authOptions: NextAuthOptions = {
               path: '/',
               domain: "."+ hostName,
               secure: useSecureCookies,
-              
-             
             }
           }, 
     },
     callbacks: {
+        /**
+         * Callback function for manipulating the JWT token.
+         */
         async jwt({token , user }) {
             if(user){
                 token.id = user.id;
-                console.log("testdsaf");
                 token.isAdmin = await fetchIsAdmin(user.id);
             }
             return token;
         },
+        /**
+         * Callback function for manipulating the session object.
+         */
         async session({ session, token }) {
             if (session.user) {
-            
                 const user = session.user as ExtendedUser;
-                user.isAdmin = token.isAdmin as boolean; // Cast token.isAdmin to boolean
-                console.log("Session callback")
-
+                // Cast token.isAdmin to boolean
+                user.isAdmin = token.isAdmin as boolean;
             }
             return session;
         }
     }
-  
-}
+};
+
+/**
+ * Fetches the isAdmin status for a given user.
+ * @param userId - The ID of the user.
+ * @returns A Promise that resolves to a boolean indicating whether the user is an admin or not.
+ */
 
 async function fetchIsAdmin(userId: string) : Promise<boolean> {
     const response = await fetch(`http://secodash.com:3000/api/isAdmin/${userId}`);
     const data = await response.json();
-    console.log("jwt callback")
     console.log(data.isAdmin);
     return data.isAdmin;
 }
 
-/*
- cookies: {
-        sessionToken: {
-            name: `${cookiePrefix}next-auth.session-token`,
-            options: {
-                httpOnly: true,
-                sameSite: "lax",
-                path: "/",
-                //domain: "."+ hostName,
-                secure: useSecureCookies,
-            }
-        },
-       
-    }
-
-*/
