19d09b3bfe4a80650881ed6c5899a68c60dcd07d
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-12-12 16:47:40 +0100

    style(backend): inline attributes

--- back-end/back-end/Dtos/Contributors/TopContributorDto.cs
+++ back-end/back-end/Dtos/Contributors/TopContributorDto.cs
@@ -4,8 +4,6 @@ namespace SECODashBackend.Dtos.Contributors;
 
 public class TopContributorDto
 {
-    [DataMember(Name = "login")]
-    public required string Login { get; init; }
-    [DataMember(Name = "contributions")]
-    public int Contributions { get; init; }
+    [DataMember(Name = "login")] public required string Login { get; init; }
+    [DataMember(Name = "contributions")] public int Contributions { get; init; }
 }
