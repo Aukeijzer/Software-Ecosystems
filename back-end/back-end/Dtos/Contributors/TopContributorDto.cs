d690d1de2d15b9228e9d4e87febf28fc3d58fa87
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-12-12 16:50:59 +0100

    chore(backend): add comment to TopContributorDto class

--- back-end/back-end/Dtos/Contributors/TopContributorDto.cs
+++ back-end/back-end/Dtos/Contributors/TopContributorDto.cs
@@ -2,6 +2,9 @@
 
 namespace SECODashBackend.Dtos.Contributors;
 
+/// <summary>
+/// Represents a data transfer object for a Top Contributor of an ecosystem.
+/// </summary>
 public class TopContributorDto
 {
     [DataMember(Name = "login")] public required string Login { get; init; }
