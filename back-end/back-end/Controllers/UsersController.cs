d9c50746f775a07a9314419e2c3a195495449216
ian van uunen <i.r.vanuunen@students.uu.nl>
2023-12-14 14:29:17 +0100

    feat: add login request call to api

--- back-end/back-end/Controllers/UsersController.cs
+++ back-end/back-end/Controllers/UsersController.cs
@@ -3,34 +3,45 @@ using SECODashBackend.Models;
 using SECODashBackend.Services.Users;
 
 namespace SECODashBackend.Controllers;
-
+/// <summary>
+/// This controller is responsible for handling requests related to users.
+/// </summary>
+/// <param name="logger"></param>
+/// <param name="usersService"></param>
 [ApiController]
 [Route("[controller]")]
 public class UsersController(ILogger<UsersController> logger, UsersService usersService) : ControllerBase
 {
-    [HttpGet]
+    [HttpGet("GetAll")]
     public async Task<List<User>> GetAllAsync()
     {
         var result = await usersService.GetAllAsync();
         return result;
     }
 
-    [HttpPost("Add User")]
-    public async Task AddUserAsync(string[] user)
+    [HttpPost("AddUser")]
+    public async Task AddUserAsync(string id, string userName)
     {
-        await usersService.AddUserAsync(user);
+        await usersService.AddUserAsync(id, userName);
     }
 
-    [HttpPost("Get By Name") ]
+    [HttpPost("GetByName") ]
     public async Task<ActionResult<User>> GetByNameAsync(string userName)
     {
         var result = await usersService.GetByNameAsync(userName);
         return result == null ? NotFound() : Ok(result);
     }
 
-    [HttpPost("Remove User")]
+    [HttpPost("RemoveUser")]
     public async Task RemoveUser(string id)
     {
         await usersService.RemoveById(id);
     }
+
+    [HttpPost("LoginRequest")]
+    public async Task<string> LoginRequest(string id, string userName)
+    {
+        return await usersService.LoginRequest(id, userName);
+    }
+    
 }
--- back-end/back-end/Services/Users/UsersService.cs
+++ back-end/back-end/Services/Users/UsersService.cs
@@ -3,9 +3,13 @@ using SECODashBackend.Database;
 using SECODashBackend.Models;
 
 namespace SECODashBackend.Services.Users;
-
+/// <summary>
+/// This service is responsible for handling all user-related requests.
+/// </summary>
+/// <param name="userContext">Is used to interact with the database.</param>
 public class UsersService(UserContext userContext)
 {
+    //Retrieve all users from the database.
     public async Task<List<User>> GetAllAsync()
     {
         var users = await userContext.Users
@@ -13,14 +17,15 @@ public class UsersService(UserContext userContext)
             .ToListAsync();
         return users;
     }
-
-    public async Task AddUserAsync(string[] user)
+    //Add a user to the database using a given id and username.
+    //All new users start as User.
+    public async Task AddUserAsync(string id, string userName)
     {
         userContext.Users.Add(new User
         {
-            Id = user[0],
-            UserName = user[1],
-            UserType = user[2]
+            Id = id,
+            UserName = userName,
+            UserType = "User"
         });
         await userContext.SaveChangesAsync();
     }
@@ -34,8 +39,20 @@ public class UsersService(UserContext userContext)
 
     public async Task RemoveById(string id)
     {
-        User user = new User{Id = id,UserName = "",UserType =""};
+        var user = new User{Id = id,UserName = "",UserType =""};
         userContext.Remove(user);
         await userContext.SaveChangesAsync();
+    }
+
+    public async Task<string> LoginRequest(string id, string userName)
+    {
+        //Check if the User already exists.
+        var user = await userContext.Users.AsNoTracking().SingleOrDefaultAsync(e => e.Id == id);
+        //Return the UserType,
+        if (user != null) return user.UserType;
+        //Or create a new User from the Id and UserName, and return the default value.
+        await AddUserAsync(id, userName);
+        return "User";
+
     }
 }
