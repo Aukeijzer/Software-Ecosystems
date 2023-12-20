a6657f550370e49ff3fda1fd31ed50d2b728644e
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2023-12-20 16:24:09 +0100

    feat: add ISchedulerService interface and Hangfire implementation

---
+++ back-end/back-end/Services/Scheduler/HangfireSchedulerService.cs
@@ -0,0 +1,33 @@
+﻿using Hangfire;
+using SECODashBackend.Services.Projects;
+
+namespace SECODashBackend.Services.Scheduler;
+
+/// <summary>
+/// Implementation of <see cref="ISchedulerService"/> using Hangfire.
+/// </summary>
+public class HangfireSchedulerService(
+    ILogger<HangfireSchedulerService> logger,
+    IRecurringJobManager recurringJobManager,
+    IProjectsService projectsService) : ISchedulerService
+{
+
+    public string AddRecurringTopicMiningJob(string topic, int amount, MiningFrequency miningFrequency)
+    {
+        var jobId = $"topic-mining_topic={topic}";
+        recurringJobManager.AddOrUpdate(jobId,() => projectsService.MineByTopicAsync("python", 10), GetCronFrequency(miningFrequency));
+        logger.LogInformation($"Job Id: {jobId} added...");
+        return jobId;
+    }
+    
+    private static string GetCronFrequency(MiningFrequency miningFrequency)
+    {
+        return miningFrequency switch
+        {
+            MiningFrequency.Minutely => Cron.Minutely(),
+            MiningFrequency.Hourly => Cron.Hourly(),
+            MiningFrequency.Daily => Cron.Daily(),
+            MiningFrequency.Weekly => Cron.Weekly()
+        };
+    }
+}
---
+++ back-end/back-end/Services/Scheduler/ISchedulerService.cs
@@ -0,0 +1,9 @@
+﻿namespace SECODashBackend.Services.Scheduler;
+
+/// <summary>
+/// Interface for a service that is responsible for scheduling jobs.
+/// </summary>
+public interface ISchedulerService
+{
+    public string AddRecurringTopicMiningJob(string topic, int amount, MiningFrequency miningFrequency);
+}
