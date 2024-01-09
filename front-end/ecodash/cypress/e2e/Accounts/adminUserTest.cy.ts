d88f0c5ec0a07cb8d0a9e72ba23fdcf5f2e90591
Barrel1 <daniogaming276@gmail.com>
2024-01-09 10:32:00 +0100

    fix(front-end): remove unnecessary api call
    Removes unnecessary post route on front-end

--- front-end/ecodash/cypress/e2e/Accounts/adminUserTest.cy.ts
+++ front-end/ecodash/cypress/e2e/Accounts/adminUserTest.cy.ts
@@ -1,54 +1,49 @@
 
 
 describe("Login page", () => {
-    before(() => {
-      cy.log(`Visiting https://secodash.com:3000`)
-      cy.visit("/")
-    })
-    it("Login with Google", () => {
-      const username = Cypress.env("GOOGLE_USER")
-      const password = Cypress.env("GOOGLE_PW")
-      const loginUrl = Cypress.env("SITE_NAME")
-      const cookieName = Cypress.env("COOKIE_NAME")
-      cy.get('[data-cy="loginButton"]').click();
-      const socialLoginOptions = {
-        username,
-        password,
-        loginUrl,
-        headless: false,
-        logs: false,
-        isPopup: true,
-        loginSelector: `a[href="${Cypress.env(
-          "SITE_NAME"
-        )}/api/auth/signin/google"]`,
-        postLoginSelector: '[data-cy=loggedInSelector]',
-      }
-  
-      return cy
-        .task("GoogleSocialLogin", socialLoginOptions)
-        .then((cookies : any) => {
-          cy.clearCookies()
-  
-          const cookie = cookies
-            .filter((cookie: any) => cookie.name === cookieName)
-            .pop()
-          if (cookie) {
-          
-            cy.setCookie(cookie.name, cookie.value, {
-              domain: cookie.domain,
-              expiry: cookie.expires,
-              httpOnly: cookie.httpOnly,
-              path: cookie.path,
-              secure: cookie.secure,
-            })
-  
-            // remove the two lines below if you need to stay logged in
-            // for your remaining tests
-            cy.visit("/api/auth/signout")
-           cy.get("form").submit()
-          }
+       
+        it("Login with Google", () => {
+            //cy.visit("/");
+           
+            const username = Cypress.env("GOOGLE_USER")
+            const password = Cypress.env("GOOGLE_PW")
+            const loginUrl = Cypress.env("SITE_NAME")
+            const cookieName = Cypress.env("COOKIE_NAME")
+            cy.get('[data-cy="loginButton"]').click();
+
+            const socialLoginOptions = {
+                username,
+                password,
+                loginUrl,
+                headless: false,
+                logs: true,
+                isPopup: true,
+                loginSelector: `[data-cy="googleLoginButton"]`,
+                postLoginSelector: '[data-cy="loggedInSelector"]',
+            }
+    
+            return cy
+                .task("GoogleSocialLogin", socialLoginOptions)
+                .then((cookies : any) => {
+                    cy.clearCookies()
+    
+                    const cookie = cookies
+                        .filter((cookie: any) => cookie.name === cookieName)
+                        .pop()
+                    if (cookie) {
+                       
+                        // Add a check to verify if login was successful on the popup window
+                        cy.get(socialLoginOptions.postLoginSelector).should('exist').then(() => {
+                            // remove the two lines below if you need to stay logged in
+                            // for your remaining tests
+                            cy.visit("/api/auth/signout")
+                            cy.get("form").submit()
+                        })
+                    }
+                })
+                
+                
         })
     })
-  })
 
 
