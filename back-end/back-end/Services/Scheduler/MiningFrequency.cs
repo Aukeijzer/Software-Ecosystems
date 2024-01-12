59b2d45b57c0fe17cc261d18ff3adda148532346
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2024-01-12 16:08:52 +0100

    chore: add comment to MiningFrequency enum

--- back-end/back-end/Services/Scheduler/MiningFrequency.cs
+++ back-end/back-end/Services/Scheduler/MiningFrequency.cs
@@ -1,5 +1,8 @@
 ﻿namespace SECODashBackend.Services.Scheduler;
 
+/// <summary>
+/// Enum that represents the frequency of mining projects.
+/// </summary>
 public enum MiningFrequency
 {
     // TODO: remove Minutely in production
