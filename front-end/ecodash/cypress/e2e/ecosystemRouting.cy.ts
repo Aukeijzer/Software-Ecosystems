e8eded23c70d76158e8c4f8252be76a0ceadf6c1
Barrel1 <daniogaming276@gmail.com>
2024-01-16 09:45:56 +0100

    fix(front-end): repair broken test
    Repairs two broken tests.

--- front-end/ecodash/cypress/e2e/ecosystemRouting.cy.ts
+++ front-end/ecodash/cypress/e2e/ecosystemRouting.cy.ts
@@ -9,3 +9,17 @@ describe('ecosystem routing test', () => {
     })
 })
 
+describe('ecosystem remove topic routing test', () => {
+    it('Clicks on a topic', () => {
+        cy.visit('http://localhost:3000/agriculture')
+        cy.fixture('apiCallEcosystem.json').then((json) => {
+            cy.intercept('POST', 'http://localhost:5003/ecosystems', json)
+        })
+        cy.contains('farming').click()
+        cy.url().should('include', 'farming')
+        cy.contains('X farming').click()
+        cy.url().should('not.include', 'farming')
+    })
+})
+
+
