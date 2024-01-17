47da97b06bacf7c52e26d71a5143fe1f51e7bb16
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2024-01-17 18:08:59 +0100

    feat(backend): add TopProjectDto class

---
+++ back-end/back-end/Dtos/Project/TopProjectDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Serialization;
+
+namespace SECODashBackend.Dtos.Project;
+
+public class TopProjectDto
+{
+   [DataMember(Name = "name")] public required string Name { get; set; }
+   [DataMember(Name = "owner")] public required string Owner { get; set; }
+   [DataMember(Name = "numberOfStars")] public int NumberOfStars { get; set; }
+}
