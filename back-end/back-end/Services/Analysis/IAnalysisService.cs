07332de9a1882c7efeadd72834ecb30221d103d7
Vivian <viv16mei@gmail.com>
2024-01-18 13:16:04 +0100

    style: added comment

--- back-end/back-end/Services/Analysis/IAnalysisService.cs
+++ back-end/back-end/Services/Analysis/IAnalysisService.cs
@@ -11,7 +11,7 @@ public interface IAnalysisService
     /// Analyzes the ecosystem given by the topics.
     /// </summary>
     /// <param name="topics">The topics of the ecosystem.</param>
-    /// <param name="technologies">The technologies of the</param>
+    /// <param name="technologies">The technologies of the ecosystem.</param>
     /// <param name="numberOfTopLanguages">The number of top languages to get.</param>
     /// <param name="numberOfTopSubEcosystems">The number of top sub ecosystems to get.</param>
     /// <param name="numberOfTopContributors">The number of top contributors to get.</param>
