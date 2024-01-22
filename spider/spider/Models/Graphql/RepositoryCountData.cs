8cd31d8187ca120bb56966ba741743f3f4d19841
Quentin Kopp <q.f.kopp@students.uu.nl>
2024-01-22 11:43:15 +0100

    fix(spider): resolve pull request comments

--- spider/spider/Models/Graphql/RepositoryCountData.cs
+++ spider/spider/Models/Graphql/RepositoryCountData.cs
@@ -1,5 +1,8 @@
 namespace spider.Models.Graphql;
 
+/// <summary>
+/// The structure of the data returned by the GitHub Graphql API when searching for how many repositories are in a search
+/// </summary>
 public class RepositoryCountData
 {
     public int? RepositoryCount { get; init; }
--- spider/spider/Services/SpiderProjectService.cs
+++ spider/spider/Services/SpiderProjectService.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Net;
 using System.Text.Json;
 using spider.Converters;
@@ -44,7 +43,7 @@ public class SpiderProjectService : ISpiderProjectService
         }
         var max = mostStarred.Search.Nodes[0].StargazerCount;
         //Use binary split to split the search space into smaller chunks
-        var sections = await BinarySplit(name, amount, startCursor, 5, max);
+        var sections = await BinarySplit(name, 5, max);
         sections.Reverse();
         var split = new Queue<(int,int)>(sections);
         var result = new List<ProjectDto>();
@@ -68,7 +67,7 @@ public class SpiderProjectService : ISpiderProjectService
     /// <param name="min">The lower bound of the search space</param>
     /// <param name="max">The upper bound of the search space</param>
     /// <returns>Returns a list of lower and upper bounds that each contain no more than 1000 repositories</returns>
-    private async Task<List<(int, int)>> BinarySplit(string name, int amount, string? startCursor, int min, int max)
+    private async Task<List<(int, int)>> BinarySplit(string name, int min, int max)
     {
         // If min and max are equal we can't divide further
         if (min == max)
@@ -87,15 +86,12 @@ public class SpiderProjectService : ISpiderProjectService
         if (repoCount > 1000)
         {
             var mid = (max + min) / 2;
-            var firstHalf = await BinarySplit(name, amount, startCursor, min, mid);
-            var secondHalf = await BinarySplit(name, amount, startCursor, mid + 1, max);
+            var firstHalf = await BinarySplit(name, min, mid);
+            var secondHalf = await BinarySplit(name, mid + 1, max);
             firstHalf.AddRange(secondHalf);
             return firstHalf;
         }
-        else
-        {
-            return [(min, max)];
-        }
+        return [(min, max)];
     }
     
     /// <summary>
