a4cbc7e5827b986eb13a46de5bc9a0ec9e62e932
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2024-01-23 14:59:19 +0100

    ops(backend): update elasticsearch credentials

--- back-end/Backend.IntegrationTests/BackendWebApplicationFactory.cs
+++ back-end/Backend.IntegrationTests/BackendWebApplicationFactory.cs
@@ -17,8 +17,8 @@ public class BackendWebApplicationFactory<TProgram> : WebApplicationFactory<TPro
     {
         // Replace the standard ElasticsearchClient by one that maps Project related requests to a dedicated test index
         var settings = new ElasticsearchClientSettings(
-                "Develop:d2VzdGV1cm9wZS5henVyZS5lbGFzdGljLWNsb3VkLmNvbTo0NDMkMjYzZTU2NzA5ZDY4NDg0YmJiODQ4NzMxYTA1MjY2ZmQkYzFjM2I1ODM5MzU3NDAwODk3MmYzYTcwNmFkNTgwZDk=",
-                new ApiKey("M293QTg0d0JxTDJWOGpTQjJ2NE06UTVNd0FQcy1RRXl3MHhGTGVkOGNwZw=="))
+                "Develop:d2VzdGV1cm9wZS5henVyZS5lbGFzdGljLWNsb3VkLmNvbTo0NDMkNTRmNTgxYmI2NGE5NDdlYmJiMjZlN2Y1NDBhNTc2YmIkY2I3MmQ3YTllNTQyNDI4M2IwMDVmNjFmMDBhZTIyMWY=",
+                new ApiKey("N2JXWE5vMEIxVmhxZFhRUGhpM0k6RWt1dnNjQllRcDZSWENETGM2akJtdw=="))
             .DefaultMappingFor<ProjectDto>(i => i
                 .IndexName("integration-test-01")
             );
