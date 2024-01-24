d4d74d4b0321f6de250e69076628bfe4e542c3c1
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2024-01-24 16:57:25 +0100

    chore(backend): add comment to TopicsBucketDto

--- back-end/back-end/Dtos/TimedData/TopicsBucketDto.cs
+++ back-end/back-end/Dtos/TimedData/TopicsBucketDto.cs
@@ -3,8 +3,12 @@ using SECODashBackend.Dtos.Ecosystem;
 
 namespace SECODashBackend.Dtos.TimedData;
 
+/// <summary>
+/// Represents a data transfer object for a bucket that contains topic data for a given time period.
+/// </summary>
+[DataContract]
 public class TopicsBucketDto
 {
-    [DataMember(Name = "bucketDateLabel")] public string BucketDateLabel { get; init; }
+    [DataMember(Name = "bucketDateLabel")] public string DateLabel { get; init; }
     [DataMember(Name = "topics")] public List<SubEcosystemDto> Topics { get; init; }
 }
