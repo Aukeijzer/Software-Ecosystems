9e17490cdde1acf9803b6ec4992b93d095514c3b
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2024-01-29 14:08:51 +0100

    feat(backend): add EcosystemMetrics record

---
+++ back-end/back-end/Models/EcosystemMetrics.cs
@@ -0,0 +1,27 @@
+﻿namespace SECODashBackend.Models;
+
+/// <summary>
+/// The metrics of a top-level ecosystem.
+/// </summary>
+public record EcosystemMetrics
+{
+    /// <summary>
+    /// The total number of projects in the ecosystem.
+    /// </summary>
+    public long NumberOfProjects { get; init; }
+    
+    /// <summary>
+    /// The total number of subtopics in the ecosystem.
+    /// </summary>
+    public long NumberOfSubTopics { get; init; }
+    
+    /// <summary>
+    /// The total number of contributions to all projects in the ecosystem.
+    /// </summary>
+    public long NumberOfContributors { get; init; }
+    
+    /// <summary>
+    /// The total number of stars of all projects in the ecosystem.
+    /// </summary>
+    public long NumberOfStars { get; init; }
+}
