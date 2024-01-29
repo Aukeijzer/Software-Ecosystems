ce550dd22ff1f7e6dbdba49efa363087b1f3faeb
LCBoucher <81970912+LCBoucher@users.noreply.github.com>
2024-01-29 14:12:06 +0100

    refactor(backend): add properties to EcosystemOverviewDto

--- back-end/back-end/Dtos/Ecosystem/EcosystemOverviewDto.cs
+++ back-end/back-end/Dtos/Ecosystem/EcosystemOverviewDto.cs
@@ -8,7 +8,38 @@ namespace SECODashBackend.Dtos.Ecosystem;
 /// </summary>
 public class EcosystemOverviewDto
 {
+    /// <summary>
+    /// The name of the ecosystem.
+    /// </summary>
+    [DataMember(Name = "name")] public required string Name { get; init; }
+    
+    /// <summary>
+    /// The display name of the ecosystem.
+    /// </summary>
     [DataMember(Name = "displayName")] public string? DisplayName { get; set; }
+    
+    /// <summary>
+    /// the description of the ecosystem.
+    /// </summary>
     [DataMember(Name = "description")] public string? Description { get; set; }
-    [DataMember(Name = "numberOfStars")] public int? NumberOfStars { get; set; }
+    
+    /// <summary>
+    /// The total number of projects in the ecosystem.
+    /// </summary>
+    [DataMember(Name = "numberOfProjects")] public long? NumberOfProjects { get; set; }
+    
+    /// <summary>
+    /// The total number of subtopics in the ecosystem.
+    /// </summary>
+    [DataMember(Name = "numberOfSubTopics")] public long? NumberOfSubTopics { get; set; }
+    
+    /// <summary>
+    /// The total number of contributions to all projects in the ecosystem.
+    /// </summary>
+    [DataMember(Name = "numberOfContributors")] public long? NumberOfContributors { get; set; }
+    
+    /// <summary>
+    /// The total number of stars of all projects in the ecosystem.
+    /// </summary>
+    [DataMember(Name = "numberOfStars")] public long? NumberOfStars { get; set; }
 }
